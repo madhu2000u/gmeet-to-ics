774fe933eadcd48ff3ea0bae73ed3503e9293c8e
Sidharth Shambu <sidharthshambu00@gmail.com>
2022-01-05 23:24:19 +0530

    feat: add Oauth init

--- lib/calendar_client.dart
+++ lib/calendar_client.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:googleapis/calendar/v3.dart';
+
 class CalendarClient {
   // For storing the CalendarApi object, this can be used
   // for performing all the operations
@@ -6,11 +9,73 @@ class CalendarClient {
   // For creating a new calendar event
   Future<Map<String, String>> insert({
     @required required String title,
-    @required String description,
-    @required String location,
-    @required List<EventAttendee> attendeeEmailList,
-    @required bool shouldNotifyAttendees,
-    @required bool hasConferenceSupport,
-    @required DateTime startTime,
-    @required DateTime endTime,
-  }) async {}
+    @required required String description,
+    @required required String location,
+    @required required List<EventAttendee> attendeeEmailList,
+    @required required bool shouldNotifyAttendees,
+    @required required bool hasConferenceSupport,
+    @required required DateTime startTime,
+    @required required DateTime endTime,
+  }) async {
+    late Map<String, String> eventData;
+
+    String calendarId = "primary";
+    Event event = Event();
+
+    event.summary = title;
+    event.description = description;
+    event.attendees = attendeeEmailList;
+    event.location = location;
+
+    if (hasConferenceSupport) {
+      ConferenceData conferenceData = ConferenceData();
+      CreateConferenceRequest conferenceRequest = CreateConferenceRequest();
+      conferenceRequest.requestId =
+          "${startTime.millisecondsSinceEpoch}-${endTime.millisecondsSinceEpoch}";
+      conferenceData.createRequest = conferenceRequest;
+
+      event.conferenceData = conferenceData;
+    }
+
+    EventDateTime start = new EventDateTime();
+    start.dateTime = startTime;
+    start.timeZone = "GMT+05:30";
+    event.start = start;
+
+    EventDateTime end = new EventDateTime();
+    end.timeZone = "GMT+05:30";
+    end.dateTime = endTime;
+    event.end = end;
+
+    try {
+      await calendar.events
+          .insert(event, calendarId,
+              conferenceDataVersion: hasConferenceSupport ? 1 : 0,
+              sendUpdates: shouldNotifyAttendees ? "all" : "none")
+          .then((value) {
+        print("Event Status: ${value.status}");
+        if (value.status == "confirmed") {
+          late String joiningLink;
+          String eventId;
+
+          eventId = value.id;
+
+          if (hasConferenceSupport) {
+            joiningLink =
+                "https://meet.google.com/${value.conferenceData.conferenceId}";
+          }
+
+          eventData = {'id': eventId, 'link': joiningLink};
+
+          print('Event added to Google Calendar');
+        } else {
+          print("Unable to add event to Google Calendar");
+        }
+      });
+    } catch (e) {
+      print('Error creating event $e');
+    }
+
+    return eventData;
+  }
+}
